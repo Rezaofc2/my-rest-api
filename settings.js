391693e3d37c1819769d7d0da359a4372c6e08c4
RezaOffc <106691452+Rezaofc2@users.noreply.github.com>
2024-10-06 23:47:51 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 
 global.creator = 'RezaOffc'// yourname
-global.MONGO_DB_URI = "mongodb+srv://reyzaahari:<db_password>@cluster0.dw7ib.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" //database mongodb 
+global.MONGO_DB_URI = "mongodb+srv://reyzaahari:reza12345#@cluster0.dw7ib.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
 global.your_email = "reyzaahari@gmail.com" //email
 global.email_password = "skntahxyeruavzyw" //application password email
