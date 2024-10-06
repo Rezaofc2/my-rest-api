ce08d6fb05a79b7523d35455e5c928636fe6ac03
RezaOffc <106691452+Rezaofc2@users.noreply.github.com>
2024-10-06 22:13:16 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.creator = 'RezaOffc'// yourname
+global.MONGO_DB_URI = "mongodb+srv://ezabotmd:Xoqfa8QRKVOBHdsq@cluster0.upv8uzk.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "reyzaahari@gmail.com" //email
+global.email_password = "skntahxyeruavzyw" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
